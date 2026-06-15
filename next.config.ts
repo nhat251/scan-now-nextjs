3fd435867adbb80638118d8dc19bd2aff50df395
nhat251 <nhatnguyendev251@gmail.com>
2026-06-15 09:31:43 +0700

    Revert "fix: resolve sharp module crash on Vercel linux-x64"
    This reverts commit b940bb010fd9f3cffb44e2c6dbcbfbfb8539100c.

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,7 @@ const withNextIntl = createNextIntlPlugin("./src/i18n/request.ts");
 
 const nextConfig: NextConfig = {
   reactCompiler: true,
+  output: "standalone",
   // cho cms run local
   images: {
     remotePatterns: [
